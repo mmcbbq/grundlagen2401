f067676457a7180f2b27db5b2a08bd7c5e489299
mmcbbq <m.martinez@bbq.de>
2025-03-03 12:42:23 +0100

    min max

--- aufgaben/10_while_loop.php
+++ aufgaben/10_while_loop.php
@@ -9,3 +9,8 @@ $user = ['Donald', "Bill","George","Ronald"];
 #Schreibe eine while loop die nach einem Namen in dem user array sucht.
 # Wenn der Name gefunden wird soll die Schleife stoppen und den index ausgeben.
 # Wen der Name nicht gefunden wird PRINTE "nicht gefunden"
+
+
+$liste = [27, 29, 7, 76, 56];
+
+#finde mit einer While Schliefe  in der liste den min und den max Wert
