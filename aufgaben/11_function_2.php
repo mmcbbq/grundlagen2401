002aa96d2150eb77d77c0f21cfd4fb974ab08959
mmcbbq <m.martinez@bbq.de>
2025-03-05 09:10:14 +0100

    aufgabe function

---
+++ aufgaben/11_function_2.php
@@ -0,0 +1,5 @@
+<?php
+# Schreibe eine Funktion multiplikation.
+# Sie soll 2 int parameter haben.
+#Wenn man nur einen Parameter uebergibt soll dieser mit 2 multipliziert werden
+#wenn man 2 Paramter uebergibt sollen diese miteinander multipliziert werden.
--- vorlesung/11_function.php
+++ vorlesung/11_function.php
@@ -1,12 +1,5 @@
-<?php
+<?php declare(strict_types=1);
 
-function berechneQuadrat($seitelaenge)
-
-{
-    $a = $seitelaenge ** 2;
-    print $a;
-}
-berechneQuadrat(5);
 
 function halloWelt()
 {
@@ -17,16 +10,42 @@ function halloWelt()
 halloWelt();
 
 
-
 function say_hallo($fname, $lname, $email)
 {
     print(" Hallo $fname $lname \n");
     print(" email: $email");
-
 }
 $name = "Bernd";
 
 say_hallo('Bernd' ,'Berndson','Bernd@bbq.de');
 
 
+function sag_name($name = "Joe")
+{
+    print "\n".$name;
+}
 
+sag_name();
+sag_name("Donald");
+
+
+function sag_viele_namen(...$name)
+{
+
+    for ($i = 0; $i < count($name); $i++) {
+        print "\n".$name[$i];
+
+    }
+}
+print "\n";
+sag_viele_namen("joe", 'donald',"ronald");
+
+
+
+
+
+function datatype(int $int) :int
+{
+    return $int;
+}
+datatype(55);
