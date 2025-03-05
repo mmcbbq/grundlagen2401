fce4de04c7182c878fbcd02c5269ab34c12cb5ee
mmcbbq <m.martinez@bbq.de>
2025-03-05 09:19:13 +0100

    aufgabe function_2

--- aufgaben/11_function_2.php
+++ aufgaben/11_function_2.php
@@ -3,3 +3,41 @@
 # Sie soll 2 int parameter haben.
 #Wenn man nur einen Parameter uebergibt soll dieser mit 2 multipliziert werden
 #wenn man 2 Paramter uebergibt sollen diese miteinander multipliziert werden.
+
+
+
+#Schreibe eine Funktion circle_area.
+# Sie soll die Flaeche eines Kreises berechnen.
+# Es soll moeglich sein den Radius oder den Durchmesser zu uebergeben.
+
+
+# Schreibe eine Funktion chess_board
+# param 1:string white_field
+# param 2:string black_field
+# param 3: int width
+# param 4: int length
+# Es soll ein Schachbrettmuster ausgegeben werden
+
+# white_field = "a"
+# black_field = "b"
+# width = 3
+# length = 4
+
+# ausgabe
+
+# a b a
+# b a b
+# a b a
+# b a b
+
+
+
+
+
+
+
+
+
+
+
+
