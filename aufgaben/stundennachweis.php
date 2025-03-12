e021e33be63cacb83db0863f6835829ceac1a071
mmcbbq <m.martinez@bbq.de>
2025-03-12 12:10:26 +0100

    stundennachweis.php

--- aufgaben/stundennachweis.php
+++ aufgaben/stundennachweis.php
@@ -1,8 +1,11 @@
 
 <?php
 
-function create_Stundennachweis(int $tage):void
+function create_Stundennachweis(int $monat,int $year, string $anfang, string $ende,string $bemerkung = ""):void
 {
+    $date = new DateTime("$year-$monat-1");
+
+    $tage_im_monat = $date->format('t');
 
     echo "<table>";
 
@@ -23,21 +26,41 @@ function create_Stundennachweis(int $tage):void
 
 
 
-    for ($i = 0; $i < $tage; $i++) {
-        $tag = $i + 1;
+    for ($i = 1; $i <= $tage_im_monat; $i++) {
+
+        $date_for_weekend = new DateTime("$year-$monat-$i");
+
+        $we = $date_for_weekend->format('N');
+        print $we;
+
+
         if ($i % 2 == 0){
             $color = $color_1;
         } else{
             $color = $color_2;
         }
+        $value_anfang = $anfang;
+        $value_ende = $ende;
+        $value_bemerkung = $bemerkung;
+
+        if ($we == 6 or $we == 7){
+            $color ='grey';
+            $value_anfang = '';
+            $value_ende = "";
+            $value_bemerkung = '';
+        }
+
+
+
+
 
         echo "<tr style='background-color: $color'>";
-        echo "<td>$tag</td>";
-        echo '<td></td>';
+        echo "<td>$i</td>";
+        echo "<td>$value_anfang</td>";
         echo '<td>-</td>';
+        echo "<td>$value_ende</td>";
         echo '<td></td>';
-        echo '<td></td>';
-        echo '<td></td>';
+        echo "<td>$value_bemerkung</td>";
         echo '<td></td>';
         echo '</tr>';
     }
@@ -62,7 +85,7 @@ function create_Stundennachweis(int $tage):void
 </head>
 <body>
 <?php
-create_Stundennachweis(31);
+create_Stundennachweis(2,2024,'8:30', '16:00','viel arbeit');
 ?>
 </body>
 </html>
